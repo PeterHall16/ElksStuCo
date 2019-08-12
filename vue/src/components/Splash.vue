f9fbb3a9c9fbc79dd521dc45650bc0f1ba51ed97
Peter Hall <peterhall330@outlook.com>
2019-08-11 23:09:03 -0400

    responsiveness

--- vue/src/components/Splash.vue
+++ vue/src/components/Splash.vue
@@ -28,15 +28,31 @@ export default {
 
 h1 {
     margin: 5vw;
-    width: 60%;
+    padding: 3vw;
+    width: 100%;
+    word-spacing: 9999rem;
     text-align: left;
-    font-size: 45pt;
+    font-size: 35pt;
     font-family: 'Montserrat', sans-serif;
     font-weight: 800;
     color: #2c3e50;
+    /* background: #FFF; */
     text-shadow: 0px 0px 10px rgba(0,0,0,.1)
     /* border: 1px solid red; */
 }
 
+@media (min-width: 800px) {
+    h1 {
+        width: 50%;
+        font-size: 35pt;
+    }
+}
+
+@media (min-width: 300px) {
+    h1 {
+        font-size: 30pt;
+    }
+}
+    
 </style>
 
